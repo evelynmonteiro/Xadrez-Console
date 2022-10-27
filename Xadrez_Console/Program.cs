522388518b7285d6705fbca8c6d5ae549592d74a
Evelyn Monteiro <evelynsilmonteiro@gmail.com>
2022-10-26 21:48:05 -0300

    Projeto realizado até aula 167

---
+++ Xadrez_Console/Program.cs
@@ -0,0 +1,25 @@
+﻿using tabuleiro;
+using xadrez;
+
+namespace Xadrez_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try{
+
+                Tabuleiro tab = new Tabuleiro(8,8);
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0,0));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2,4));
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3,6));
+                Tela.imprimirTabuleiro(tab);
+                Console.ReadLine();
+            }
+            catch(TabuleiroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Xadrez_Console/tabuleiro/TabuleiroException.cs
@@ -0,0 +1,10 @@
+namespace tabuleiro
+{
+    class TabuleiroException : ApplicationException
+    {
+        public TabuleiroException(string msg) : base(msg)
+        {
+
+        }
+    }
+}
